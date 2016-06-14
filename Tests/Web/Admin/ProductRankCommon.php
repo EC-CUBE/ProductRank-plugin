29c03339b13c790b056f185781832bb11d493448
lqdung <lqdung@lockon-vn.com>
2016-06-14 14:56:14 +0700

    [ADD] Add test unit. Test coverage > 80%.

---
+++ Tests/Web/Admin/ProductRankCommon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lqdung
+ * Date: 6/14/2016
+ * Time: 10:08 AM
+ */
+
+namespace Plugin\ProductRank\Tests\Web\Admin;
+
+
+use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
+
+class ProductRankCommon extends AbstractAdminWebTestCase
+{
+    
+}
