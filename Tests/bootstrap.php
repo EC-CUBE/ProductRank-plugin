6b36a861f9b70ac49f25b3dc9c073ab66dd4f258
ndquocphong <ndquocphong@gmail.com>
2018-07-12 15:53:30 +0700

    fix phpcs

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -12,6 +12,7 @@
  */
 
 $loader = require __DIR__.'/../../../../vendor/autoload.php';
+
 $envFile = __DIR__.'/../../../../.env';
 if (file_exists($envFile)) {
     (new \Symfony\Component\Dotenv\Dotenv())->load($envFile);
