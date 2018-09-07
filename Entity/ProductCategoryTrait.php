1aaee7f9313b53ba83944947706b425ca4549d07
ndquocphong <ndquocphong@gmail.com>
2018-09-07 13:35:46 +0700

    Fix phpcs

--- Entity/ProductCategoryTrait.php
+++ Entity/ProductCategoryTrait.php
@@ -1,4 +1,16 @@
 <?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Plugin\ProductRank\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -28,11 +40,13 @@ trait ProductCategoryTrait
      * Set product_rank_sort_no
      *
      * @param $productRankSortNo
+     *
      * @return $this
      */
     public function setProductRankSortNo($productRankSortNo)
     {
         $this->product_rank_sort_no = $productRankSortNo;
+
         return $this;
     }
 }
